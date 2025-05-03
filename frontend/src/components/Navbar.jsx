866cd5cf66dabc1cba0cdb8b0f2d48b976a0cb2e
unknown <jimmykimeu042@gmail.com>
2025-05-03 12:55:02 +0300

    Update Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import house from "../assets/house.svg";
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse  } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 const NavLinks = () => {
   return (
     <>
-    <NavLink to="/pages/Dashboard.jsx"> <img height={50} src={house} alt='Home'/> </NavLink>
-    <NavLink to="/pages/Group.jsx">Group</NavLink>
-    <NavLink to="/pages/Profile.jsx">Profile</NavLink>
+    <NavLink to="/pages/Dashboard.jsx"> <FontAwesomeIcon icon={faHouse} /> </NavLink>
+    <NavLink to="/pages/Group.jsx"><FontAwesomeIcon icon={faUsers} /> </NavLink>
+    <NavLink to="/pages/Profile.jsx"><FontAwesomeIcon icon={faUser} /> </NavLink>
     </>
   )
     
@@ -17,7 +22,7 @@ const NavLinks = () => {
 
 const Nav = () => {
 return (
-  <nav className="w-1/3">
+  <nav className="w-1/3 fixed bottom-0 bg-white border-t border-gray-200 shadow-lg inline-flex justify-between items-center p-4">
   <div className="flex justify-between">
   <NavLinks />
   </div>
