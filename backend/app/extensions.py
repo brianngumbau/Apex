6108df252b803ef290bd86e886522c676b669610
brianngumbau <brianngumbau9075@gmail.com>
2025-09-11 22:34:27 +0300

    update

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,7 +1,7 @@
 # extensions.py
 from flask_jwt_extended import JWTManager
 from flask_socketio import SocketIO
-from models import db
+from app.models import db
 
 jwt = JWTManager()
 socketio = SocketIO(cors_allowed_origins="*")
